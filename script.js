f583ef79d3a07a7e07ff7f31d10d1d11ecb6c84e
DiveshPunjabi1 <diveshpunabi90@gmail.com>
2024-04-04 19:57:42 +0530

    update sw

--- script.js
+++ script.js
@@ -1,32 +1,129 @@
+// 'use strict';
+
+// /**
+//  * navbar toggle
+//  */
+
+// const overlay = document.querySelector("[data-overlay]");
+// const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+// const navbar = document.querySelector("[data-navbar]");
+// const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+// const navElemArr = [overlay, navOpenBtn, navCloseBtn];
+
+// for (let i = 0; i < navElemArr.length; i++) {
+//   navElemArr[i].addEventListener("click", function () {
+//     navbar.classList.toggle("active");
+//     overlay.classList.toggle("active");
+//   });
+// }
+
+
+
+// /**
+//  * add active class on header when scrolled 200px from top
+//  */
+
+// const header = document.querySelector("[data-header]");
+
+// window.addEventListener("scroll", function () {
+//   window.scrollY >= 200 ? header.classList.add("active")
+//     : header.classList.remove("active");
+// })
 'use strict';
 
+
+
 /**
- * navbar toggle
+ * add event on element
  */
 
-const overlay = document.querySelector("[data-overlay]");
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbar = document.querySelector("[data-navbar]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-
-const navElemArr = [overlay, navOpenBtn, navCloseBtn];
-
-for (let i = 0; i < navElemArr.length; i++) {
-  navElemArr[i].addEventListener("click", function () {
-    navbar.classList.toggle("active");
-    overlay.classList.toggle("active");
-  });
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
 }
 
 
 
 /**
- * add active class on header when scrolled 200px from top
+ * navbar toggle
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header sticky & back top btn active
  */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-window.addEventListener("scroll", function () {
-  window.scrollY >= 200 ? header.classList.add("active")
-    : header.classList.remove("active");
-})
+const headerActive = function () {
+  if (window.scrollY > 150) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+let lastScrolledPos = 0;
+
+const headerSticky = function () {
+  if (lastScrolledPos >= window.scrollY) {
+    header.classList.remove("header-hide");
+  } else {
+    header.classList.add("header-hide");
+  }
+
+  lastScrolledPos = window.scrollY;
+}
+
+addEventOnElem(window, "scroll", headerSticky);
+
+
+
+/**
+ * scroll reveal effect
+ */
+
+const sections = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+  for (let i = 0; i < sections.length; i++) {
+    if (sections[i].getBoundingClientRect().top < window.innerHeight / 2) {
+      sections[i].classList.add("active");
+    }
+  }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
